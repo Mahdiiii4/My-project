28b9c7708b7b2ec745242a6a84d3063dac08c821
Adam <adfr06001@utb.vaxjo.se>
2023-09-07 14:02:00 +0200

    My spel
    Cube movement

--- Assets/MovementCube.cs
+++ Assets/MovementCube.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovementCube : MonoBehaviour
 {
-    float timer = 0;
     public float speed = 1f;
+    bool moveUpZ = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,26 @@ public class MovementCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new UnityEngine.Vector3(0,0,speed * Time.deltaTime);
+        if(transform.position.z <= 7 && !moveUpZ)
+        {
+            transform.position += new Vector3(0,0,speed * Time.deltaTime);
+        }
+        else if(transform.position.x >=-7)
+        {
+            moveUpZ= true;
+            transform.position += new Vector3(-speed * Time.deltaTime,0,0);
+        }
+
+        moveUpZ= false;
+
+        else if(transform.position.z >=-7 && !moveUpZ)
+        {
+            transform.position += new Vector3(0,0,-speed * Time.deltaTime);
+        }
+        else if(transform.position.x <=-7 )
+        {
+            moveUpZ= true;
+            transform.position += new Vector3(speed * Time.deltaTime,0,0);
+        }
     }
 }
